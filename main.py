bcc38d331bb16efd0f0ba669cab0034ca14d1b70
Kevin Frans <kevin@bobthechicken.com>
2016-06-09 11:12:20 -0700

    added network model

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow.python.platform import gfile
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('train_dir', '/Users/kevin/Documents/Python/taco/training/train',
+tf.app.flags.DEFINE_string('train_dir', '/Users/kevin/Documents/Python/feature-visualization/training/train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 
@@ -23,3 +23,57 @@ def conv2d(x,W):
 # max pooling basically shrinks it by 2x, taking the highest value on each feature.
 def maxPool2d(x):
     return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME")
+
+
+batchsize = 50;
+imagesize = 32;
+colors = 3;
+
+def train():
+    sess = tf.InteractiveSession()
+
+    img = tf.placeholder("float",shape=[None,imagesize*imagesize*colors])
+    lbl = tf.placeholder("float",shape=[None,10])
+    xImage = tf.reshape(img,[-1,imagesize,imagesize,colors])
+    # for each 5x5 area, check for 32 features over 3 color channels
+    wConv1 = initWeight([5,5,colors,32])
+    bConv1 = initBias([32])
+    # move the conv filter over the picture
+    conv1 = conv2d(xImage,wConv1)
+    # adds bias
+    bias1 = conv1 + bConv1
+    # relu = max(0,x), adds nonlinearality
+    relu1 = tf.nn.relu(bias1)
+    # maxpool to 16x16
+    pool1 = maxPool2d(relu1)
+    # second conv layer, takes a 16x16 with 32 layers, turns to 8x8 with 64 layers
+    wConv2 = initWeight([5,5,32,64])
+    bConv2 = initBias([64])
+    conv2 = conv2d(pool1,wConv2)
+    bias2 = conv2 + bConv2
+    relu2 = tf.nn.relu(bias2)
+    pool2 = maxPool2d(relu2)
+    # fully-connected is just a regular neural net: 8*8*64 for each training data
+    wFc1 = initWeight([(imagesize/4) * (imagesize/4) * 64, 1024])
+    bFc1 = initBias([1024])
+    # reduce dimensions to flatten
+    pool2flat = tf.reshape(pool2, [-1, (imagesize/4) * (imagesize/4) *64])
+    # 128 training set by 2304 data points
+    fc1 = tf.matmul(pool2flat,wFc1) + bFc1;
+    relu3 = tf.nn.relu(fc1);
+    # dropout removes duplicate weights
+    keepProb = tf.placeholder("float");
+    drop = tf.nn.dropout(relu3,keepProb);
+    wFc2 = initWeight([1024,10]);
+    bFc2 = initWeight([10]);
+    # softmax converts individual probabilities to percentages
+    guesses = tf.nn.softmax(tf.matmul(drop, wFc2) + bFc2);
+    # how wrong it is
+    cross_entropy = -tf.reduce_sum(lbl*tf.log(guesses + 1e-9));
+    # theres a lot of tensorflow optimizers such as gradient descent
+    # adam is one of them
+    optimizer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy);
+    # array of bools, checking if each guess was correct
+    correct_prediction = tf.equal(tf.argmax(guesses,1), tf.argmax(lbl,1));
+    # represent the correctness as a float [1,1,0,1] -> 0.75
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"));
